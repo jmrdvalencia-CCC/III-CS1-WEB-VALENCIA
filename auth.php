53e1bb0e70d0e592a3ac108562298eba24864d48
Joni <jdvalenccia@diwamail.com>
2025-02-22 09:56:15 +0800

    valencia new file for auth

--- auth.php
+++ auth.php
@@ -1,4 +1,46 @@
 <?php
-echo "You are login. please checkout for update";
+
+$loginCredentials =[
+    [
+        "username"=>"admin",
+        "password"=>"1234",
+        "role"=>"admin"
+    ],
+    [
+        "username"=>"user",
+        "password"=>"4321",
+        "role"=>"admin"
+    ]
+
+    ];
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+
+foreach ($loginCredentials as $loginCredential)
+if  ($loginCredential["username"] == $username)
+    {
+           if($loginCredential["password"] == $password)
+           {
+                echo "Authenticated";
+                header("location:home.php");
+                break;
+           }
+           else{
+                echo  "Invalid Password";
+                header("location:index.php");
+                break;
+           }
+        }
+else
+{
+    
+}
+echo  "Invalid username";
+
+
+}
 
 ?>
